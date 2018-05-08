b2c5439098aa12d089edf032219d4f634055a4b6
Louis <lrflrf@sbcglobal.net>
2018-05-08 15:00:07 -0500

    Initial Check In

---
+++ src/com/arkansascodingacademy/Sudoku.java
@@ -0,0 +1,14 @@
+package com.arkansascodingacademy;
+
+import java.util.*;
+
+public class Sudoku
+{
+    private static final int ROW_COUNT = 9;
+    private static final int COLUMN_COUNT = 9;
+
+    public static int[][] solve(int[][] puzzle)
+    {
+        return null;
+    }
+}
